c3494012ebb5a934aa8e3bff100e75e31e2c89f8
aravjain63 <44543506+aravjain63@users.noreply.github.com>
2018-11-11 16:57:46 +0530

    Add files via upload

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 
 [...]
 """
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
